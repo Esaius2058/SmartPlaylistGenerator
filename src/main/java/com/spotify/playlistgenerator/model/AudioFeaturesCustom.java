26f832846efba38c720d11450dd349d3c828a377
Esaius2058 <wabwirewild@gmail.com>
2025-07-08 23:50:59 +0300

    feat: add AudioFeaturesCustom model with tempo, energy, valence, and danceability
    chore: implement Serializable in AudioFeaturesCustom for safe transport or caching
    
    refactor: encapsulate AudioFeaturesCustom fields with private getters and setters

--- src/main/java/com/spotify/playlistgenerator/model/AudioFeaturesCustom.java
+++ src/main/java/com/spotify/playlistgenerator/model/AudioFeaturesCustom.java
@@ -1,15 +1,41 @@
 package com.spotify.playlistgenerator.model;
 
-public class AudioFeatures {
+import java.io.Serializable;
+
+public class AudioFeaturesCustom implements Serializable {
     private String id;
     private double tempo;
     private double danceability;
     private double energy;
+    private double valence;
 
-    public AudioFeatures(String id, double tempo, double danceability, double energy){
+    public AudioFeaturesCustom(String id, double tempo, double danceability, double energy, double valence){
         this.id = id;
         this.tempo = tempo;
         this.danceability = danceability;
         this.energy = energy;
+        this.valence = valence;
     }
+
+    public String getId() {return id;}
+    public double getTempo() {
+        return tempo;
+    }
+    public double getDanceability(){
+        return danceability;
+    }
+    public double getEnergy(){
+        return energy;
+    }
+    public double getValence(){return valence; }
+
+    public void setId(String id) {this.id = id; }
+    public void setTempo(double tempo) {
+        this.tempo = tempo;
+    }
+    public void setDanceability(double danceability){
+        this.danceability = danceability;
+    }
+    public void setEnergy(double energy){ this.energy = energy; }
+    public void setValence(double valence){ this.valence = valence; }
 }
